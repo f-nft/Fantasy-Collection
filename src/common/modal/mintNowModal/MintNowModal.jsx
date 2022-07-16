d7d4d9b27e3431a5d26d0588f02fb7684158f91a
f-nft <pnft56061@gmail.com>
2022-07-16 12:59:52 +0700

    update contents

---
+++ src/common/modal/mintNowModal/MintNowModal.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { useModal } from "../../../utils/ModalContext";
+import { FiX } from "react-icons/fi";
+import Button from "../../button";
+import MintModalStyleWrapper from "./MintNow.style";
+import mintImg from "../../../assets/images/icon/fnft.gif";
+import hoverShape from "../../../assets/images/icon/hov_shape_L.svg";
+import { MdPriceChange } from "react-icons/md";
+
+const MintNowModal = () => {
+  const [count, setCount] = useState(1);
+  const { mintModalHandle } = useModal();
+  return (
+    <>
+      <MintModalStyleWrapper className="modal_overlay">
+        <div className="mint_modal_box">
+          <div className="mint_modal_content">
+            <div className="modal_header">
+              <h2>Collect YOUR NFT before end</h2>
+              <button onClick={() => mintModalHandle()}>
+                <FiX />
+              </button>
+            </div>
+            <div className="modal_body text-center">
+              <div className="mint_img">
+                <img src={mintImg} alt="f-nft mint" />
+              </div>
+              <div className="mint_count_list">
+                <ul>
+                  <li>
+                    <h5>Remaining</h5>
+                    <h5>
+                      8,282/<span>10,000</span>
+                    </h5>
+                  </li>
+                  <li>
+                    <h5>Price</h5>
+                    <h5 id="price">0.07 ETH</h5>
+                  </li>
+                  <li>
+                    <h5>Quantity</h5>
+                    <div className="mint_quantity_sect">
+                      <button
+                        onClick={() =>
+                          count > 1 ? setCount(count - 1) : count
+                        }
+                      >
+                        -
+                      </button>
+                      <input
+                        type="text"
+                        id="quantity"
+                        value={count}
+                        onChange={(e) => setCount(e.target.value)}
+                      />
+                      <button onClick={() => setCount(count + 1)}>+</button>
+                    </div>
+
+                    <h5>
+                      <span>{MdPriceChange.count}</span> ETH
+                    </h5>
+                  </li>
+                </ul>
+              </div>
+              <div className="modal_mint_btn">
+                <Button lg variant="mint">
+                  Mint Now
+                </Button>
+              </div>
+            </div>
+
+            <div className="modal_bottom_shape_wrap">
+              <span className="modal_bottom_shape shape_left">
+                <img src={hoverShape} alt="bithu nft hover shape" />
+              </span>
+              <span className="modal_bottom_shape shape_right">
+                <img src={hoverShape} alt="bithu nft hover shape" />
+              </span>
+            </div>
+          </div>
+        </div>
+      </MintModalStyleWrapper>
+    </>
+  );
+};
+
+export default MintNowModal;
---
+++ src/components/section/banner/v1/Banner.jsx
@@ -0,0 +1,72 @@
+import { useModal } from "../../../../utils/ModalContext";
+import Counter from "../../../../common/counter";
+import Button from "../../../../common/button";
+import BannerV1Wrapper from "./Banner.style";
+
+import characterThumb from "../../../../assets/images/nft/Character1.png";
+import mintLiveDownArrow from "../../../../assets/images/nft/mint_live_down_arrow.svg";
+import mintLiveText from "../../../../assets/images/nft/mint_live_text.png";
+import homeImageBG from "../../../../assets/images/nft/home_img_bg.png";
+
+const Banner = () => {
+  const { mintModalHandle } = useModal();
+  return (
+    <BannerV1Wrapper id="home">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="bithu_v1_baner_left">
+              <h2>f-nft Fantasy 👗 NFT collections</h2>
+              <h3>
+                <span className="count">
+                  <Counter end={1} duration={25} />
+                </span>{" "}
+                / 25 Minted
+              </h3>
+              <div className="banner_buttons">
+                <Button lg variant="mint" onClick={() => mintModalHandle()}>
+                  {" "}
+                  Mint now
+                </Button>
+                <Button lg variant="outline">
+                  Wishlist now
+                </Button>
+              </div>
+              <div className="coin-info">
+                <span>Max 10 NFTs per wallet . Price 0.06 ETH + gas</span>
+                <span>
+                  MINT IS LIVE{" "}
+                  <span className="highlighted">UNTIL 01 JULY 04:00H</span>
+                </span>
+                <span>Presale : SOLDOUT</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="bithu_v1_baner_right">
+              <div className="bithu_v1_baner_right_img_sect">
+                <div className="mint_live_circle_sect">
+                  <div className="mint_live_circle">
+                    <span>
+                    </span>
+                    <span className="mint_live_text rotated-style">
+                      <img src={mintLiveText} alt="" />
+                    </span>
+                  </div>
+                </div>
+                <div className="bithu_v1_baner_right_img_bg">
+                  <img src={homeImageBG} alt="" />
+                </div>
+                <div className="bithu_v1_baner_right_img">
+                  <img src={characterThumb} alt="avater" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BannerV1Wrapper>
+  );
+};
+
+export default Banner;
