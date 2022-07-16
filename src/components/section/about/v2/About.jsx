cc9ff6c12a067d01bc60bdcc697e1fec33a7a570
f-nft <pnft56061@gmail.com>
2022-07-16 13:01:58 +0700

    Update About.jsx

--- src/components/section/about/v2/About.jsx
+++ src/components/section/about/v2/About.jsx
@@ -20,24 +20,17 @@ const About = () => {
         <div className="v2_about_us_content">
           <div className="v2_about_us_text">
             <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classNameical Latin literature from 45
-              BC, making it over 2000 years old. Richard McClintock, a Latin
-              professor at Hampden-Sydney College in Virginia, looked up one of
-              the more obscure Latin words, consectetur cites of the word in
-              classNameical literature.
+            Non-fungible Tokens (NFTs) are a method of proving digital ownership. Here are the top three things we think you should know about them as an artist or art collector so you can use them safely and easily. They are based on blockchain technology. These are transaction-recording distributed public ledgers. Each NFT is uniquely identified and stored on the blockchain with an identification code and metadata. “Metadata” means “data about data” in this context, and it is simply some additional information that describes the NFT and is stored alongside it. 
+They’re a great way to identify digital assets and keep track of their supply. Whether you’ve made a piece of music, a digital piece of art, or a video, “minting” it as an NFT allows you to prove ownership. Digital assets used to be relatively easy to steal. This is significant because it means that artists will no longer be cheated out of royalties, and collectors will no longer have to worry about investing in a fake asset.
             </p>
             <p>
-              The standard chunk of Lorem Ipsum used since the 1500s is
-              reproduced below for those interested. Sections 1.10.32 and
-              1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-              reproduced in their exact original a latin professor at
-              Hampden-Sydney
+            NFTs, also known as immutable tokens, are based on Blockchain technology. With this technology, all visual, written, and audio works can be stored in digital format. The term “next-generation token” (NFT) is used to describe assets created using blockchain technology. 
+The Ethereum blockchain contains the majority of NFTs. Ethereum is a Cryptocurrency similar to Bitcoin and Dogecoin, but it also supports non-fungible tokens (NFTs). An NFT is distinguished by the fact that it is linked to the token. The metadata on NFTs is processed using a cryptographic hash function, which is a mathematical algorithm that generates a unique string of letters and numbers. NFTs are also used to make asset interoperability possible across multiple platforms.
             </p>
 
             <div className="counter-wrap">
               <h4>
-                To be minted <Counter end="9999" />{" "}
+                To be minted <Counter end="10,000" />{" "}
               </h4>
               <h4>
                 Attributes <Counter end="555" />
