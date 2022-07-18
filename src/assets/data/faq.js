0cdd5a9c4aefe9b65d0f6f0d93cc7b6d110d302b
f-nft <pnft56061@gmail.com>
2022-07-18 17:43:11 +0700

    Update faq.js

--- src/assets/data/faq.js
+++ src/assets/data/faq.js
@@ -1,27 +1,27 @@
 const data = [
   {
-    title: "What is NFT ?",
+    title: "What is NFT?",
     text: "Non-fungible Tokens (NFTs) are a method of proving digital ownership. Here are the top three things we think you should know about them as an artist or art collector so you can use them safely and easily. They are based on blockchain technology. These are transaction-recording distributed public ledgers. Each NFT is uniquely identified and stored on the blockchain with an identification code and metadata. “Metadata” means “data about data” in this context, and it is simply some additional information that describes the NFT and is stored alongside it. They’re a great way to identify digital assets and keep track of their supply. Whether you’ve made a piece of music, a digital piece of art, or a video, “minting” it as an NFT allows you to prove ownership. Digital assets used to be relatively easy to steal. This is significant because it means that artists will no longer be cheated out of royalties, and collectors will no longer have to worry about investing in a fake asset.",
   },
   {
-    title: "How we can buy and invest NFT ?",
+    title: "How we can buy and invest NFT?",
     text: "EASY 4 STEPS Above",
   },
   {
-    title: "Why we should choose Fashion NFT ?",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    title: "Why we should choose Fashion NFT?",
+    text: "It makes the investment becomes very easy to store and portable.",
   },
   {
     title: "Where we can buy and sell NFts ?",
-    text: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur cites of the word in classical literature.",
+    text: "In all markets, NFT, or between users with user",
   },
   {
     title: "How secure is this token ?",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    text: "All our SmartContract is verified at Main network",
   },
   {
     title: "What is your contract address ?",
-    text: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur cites of the word in classical literature.",
+    text: "https://etherscan.io/token/0x6f6bd2efd2947a0a1b736131bf9a8584e4dd606c - https://polygonscan.com/token/0x01296da17460cc88fcdbfdf0196357d2f6e57472 - https://bscscan.com/token/0x3ef96a7649abee21eb2f013481b39cab92f4d10b",
   },
 ];
 
