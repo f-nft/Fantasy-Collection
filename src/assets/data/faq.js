01959cf12e1449be02f93381167377abad206ce0
f-nft <pnft56061@gmail.com>
2022-07-18 13:54:03 +0700

    update

--- src/assets/data/faq.js
+++ src/assets/data/faq.js
@@ -1,6 +1,6 @@
 const data = [
   {
-    title: "What is MiNFT ?",
+    title: "What is NFT ?",
     text: "Non-fungible Tokens (NFTs) are a method of proving digital ownership. Here are the top three things we think you should know about them as an artist or art collector so you can use them safely and easily. They are based on blockchain technology. These are transaction-recording distributed public ledgers. Each NFT is uniquely identified and stored on the blockchain with an identification code and metadata. “Metadata” means “data about data” in this context, and it is simply some additional information that describes the NFT and is stored alongside it. They’re a great way to identify digital assets and keep track of their supply. Whether you’ve made a piece of music, a digital piece of art, or a video, “minting” it as an NFT allows you to prove ownership. Digital assets used to be relatively easy to steal. This is significant because it means that artists will no longer be cheated out of royalties, and collectors will no longer have to worry about investing in a fake asset.",
   },
   {
@@ -8,11 +8,11 @@ const data = [
     text: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur cites of the word in classical literature.",
   },
   {
-    title: "Why we should choose MiNFT ?",
+    title: "Why we should choose F-NFT ?",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
-    title: "Where we can buy and sell NFts ?",
+    title: "Where we can buy and sell NFTs ?",
     text: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur cites of the word in classical literature.",
   },
   {
