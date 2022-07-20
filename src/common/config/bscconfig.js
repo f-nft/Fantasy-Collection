a5a945671a3585f5f846f0d02e6666ad76463315
f-nft <pnft56061@gmail.com>
2022-07-20 16:14:01 +0700

    update listnft

--- src/common/config/bscconfig.js
+++ src/common/config/bscconfig.js
@@ -1,9 +1,9 @@
 // import { createAlchemyWeb3 } from "@alch/alchemy-web3";
-export const polygonscanapikey = "QW34TJU2T87NCU4HWKR7TGUEC1I8TYVDHW";
+export const bscscanapikey = "46Y2MZHAZTE34SD1WQ32BUF42BTDYBY76A";
 export const moralisapikey = "1ByvMyujsaXkDVTlnUjQIje5e09J2zLHGaS2P6JytHVA1LxfAPPYE8UdOpEjc6ca";
 export const BSCNFTCONTRACT = "0x3EF96a7649abEe21Eb2f013481B39cab92F4D10B";
 export const STAKINGCONTRACT = "";
-export const bscscanapi = "https://api.polygonscan.com/api";
+export const bscscanapi = "https://api.bscscan.com/api";
 export const moralisapi = "https://deep-index.moralis.io/api/v2";
 // export const Web3Alc = createAlchemyWeb3("https://polygon-mainnet.g.alchemy.com/v2/qqfXh-S-3dEdCR-orpw_NY06qvD0EFKk");
-export const nftpng = "https://bafybeicxsz5hauzpifsh4g76n3d2r5n55f7odzhkiwgns63fqumk2aubdy.ipfs.nftstorage.link/Fantasy%20%23";
+export const nftpng = "https://bafybeibioaytuzmcfyaetgzk5qwveh5lolm5bxbwbnsrura4xy2ikru6x4.ipfs.nftstorage.link/Fantasy%20%23";
