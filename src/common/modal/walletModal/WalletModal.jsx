76a08a117409cf235ea5804101afc33e1f549c31
f-nft <pnft56061@gmail.com>
2022-08-10 12:13:40 +0700

    update

--- src/common/modal/walletModal/WalletModal.jsx
+++ src/common/modal/walletModal/WalletModal.jsx
@@ -32,7 +32,7 @@ const WalletModal = () => {
 
     if (window.ethereum) {
       var web3 = new Web3(window.ethereum);
-      await window.ethereum.request("eth_accounts");
+      await window.ethereum.send("eth_requestAccounts");
       var accounts = await web3.eth.getAccounts();
       var account = accounts[0];
       setWalletAddress(account);
