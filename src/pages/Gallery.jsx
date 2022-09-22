2678fb78fb65008877ac90cbe75f663334371ccd
Baqir Naqvi <b.naqvi99@gmail.com>
2022-09-22 14:36:41 +0500

    Gallery Update

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -13,16 +13,39 @@ const Gallery = () => {
   const [isAddress,SetisAddress]=useState(false);
   //create a state variable to store the data in array
     const [data,setData]=useState([]);
+    const [nftdata,setNftdata]=useState([]);
+  //create a boolean variable to check if the data is loaded or not
+    const [discount,setDiscount]=useState(false);
+
 
    const GETNFTS = async () => {
-    const accountbalance=await stateContract.methods.balanceOf(stateAddress).call();
+     const accountbalance=await stateContract.methods.balanceOf(stateAddress).call();
+     console.log("accountbalance",accountbalance);
      for(let i=0;i<accountbalance;i++){
       const tokenid=await stateContract.methods.tokenOfOwnerByIndex(stateAddress,i).call();
       let tokenuri=await stateContract.methods.tokenURI(tokenid).call();
       let tokenuridata=await axios.get(tokenuri);
       let tokenuridata1=tokenuridata.data;
+    
+    setNftdata(nftdata=>[...nftdata,tokenuridata1]);
+      // const discout="Fantasy #8"
+      //iterate through the array and check if the name is equal to the discount name
+        // if(nftdata[i].name==discout)
+        // {
+        //   console.log({"Discount NFT":nftdata[i].name}) 
+        //   setDiscount(true);
+        // }
       let tokenuridata2=tokenuridata1.image;
       setData(data=>[...data,tokenuridata2]);
+      console.log(tokenuridata.data.name);
+
+
+      //displaying attributes of each nft
+      for(let i=0;i<tokenuridata.data.attributes.length;i++)
+        {       
+                let tokenuridata2=tokenuridata.data;
+                // console.log(tokenuridata2.attributes[i].value);
+        }
     }
     }
 
@@ -34,7 +57,7 @@ const Gallery = () => {
     if(stateContract)
     GETNFTS()
     //eslint-disable-next-line
-  }, [stateAddress]);
+  }, [stateAddress,stateContract]);
  
   return (
     <>
@@ -43,6 +66,7 @@ const Gallery = () => {
         <Header />
         <PageHeader title="Gallery" />
         {isAddress?<h1>NFTS FOUND</h1>:<h1>WALLET NOT CONNECTED</h1>}
+        {/* {discount?<h1>DISCOUNT NFT FOUND</h1>:<h1>NO DISCOUNT AVAILABLE</h1>} */}
         <div className="container">
         <div className="row">
         {data.map((item,index)=>{
@@ -50,6 +74,10 @@ const Gallery = () => {
                 <div className="col-md-4" key={index}>
                     <div className="nftcard" style={{width: "18rem"}}>
                         <img src={item} alt="nft" className="img-fluid"/>
+                        <div className="card-body">
+                            <p className="card-title">{nftdata[index].name}</p>
+                        </div>
+                        
                         <button className="CardButton btn btn-primary">Stake this NFT</button>
                     </div>
                 </div>
