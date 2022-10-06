dbd96f87dc0315b8ab00aee40d6c307b205154ac
f-nft <pnft56061@gmail.com>
2022-10-06 10:12:54 +0700

    update

--- src/components/GalleryComponents/Features.js
+++ src/components/GalleryComponents/Features.js
@@ -6,13 +6,13 @@ export default function Features() {
    return (
       <section className="features">
         <h1 className="featurehead">
-          Token Features
+          Features
         </h1>
         <div className="banner-bottom-margin">
           <div className="banner--left">
-            <h2 className="banner__title">Token Supply</h2>
-            <p className="banner__text text text--small text--regular text--muted">
-              No future minting allowed in the contract. Tokens will be distributed to the community through giveaways,<br/> community interaction, and surprise airdrops.
+            <h2 className="banner__title">Supply</h2>
+            <p className="card__header">
+              No future minting allowed in the contract. Tokens will be distributed to the community through giveaways,<br/> community interaction, and surprise airdrops and only reward by staking NFT.
             </p>
             <div className="road-body features-card">
             <div className="featurecard card--secondary card--white">
@@ -23,19 +23,19 @@ export default function Features() {
         </div>
         <div className="featurecard card--secondary card--white">
           <div className="card__header">
-            <h3>Community</h3>
+            <h3>Communities</h3>
           </div>
           <h2>30%</h2>
         </div>
         <div className="featurecard card--white card--white">
           <div className="card__header">
-            <h3>Team</h3>
+            <h3>Teams</h3>
           </div>
           <h2>10%</h2>
         </div>
         <div className="featurecard card--white card--white">
           <div className="card__header">
-            <h3>Reward</h3>
+            <h3>Rewards</h3>
           </div>
           <h2>30%</h2>
         </div>
